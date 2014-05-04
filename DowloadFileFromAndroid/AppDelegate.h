9ea2a242df5bad9a79abcb70949fdbeba42fa39a
tigerhulu <tigerhulu@hotmail.com>
2014-05-04 15:06:12 +0800

    Initial Commit

---
+++ DowloadFileFromAndroid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DowloadFileFromAndroid
+//
+//  Created by VincentHu on 14-5-4.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
